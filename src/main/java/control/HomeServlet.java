00a07e42a437202eca8bd2dc2b6fce6744164e37
Antonio Liberti <antonioliberti2005@gmail.com>
2026-09-03 17:37:46 +0200

    Aggiunta HomeServlet

---
+++ src/main/java/control/HomeServlet.java
@@ -0,0 +1,33 @@
+package control;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import dao.ProdottoDAO;
+import model.Prodotto;
+
+@WebServlet("/Home") 
+public class HomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ProdottoDAO prodottoDAO = new ProdottoDAO();
+        List<Prodotto> catalogo = prodottoDAO.doRetrieveAll();
+        
+ 
+        request.setAttribute("prodotti", catalogo);
+        
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
--- src/main/java/dao/ProdottoDAO.java
+++ src/main/java/dao/ProdottoDAO.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author Anto127
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Prodotto;
+import model.ConPool;
+
 public class ProdottoDAO {
-    
+
+    public List<Prodotto> doRetrieveAll() {
+        List<Prodotto> prodotti = new ArrayList<>();
+        String query = "SELECT * FROM prodotto";
+        
+        try (Connection con = ConPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(query);
+             ResultSet rs = ps.executeQuery()) {
+             
+            while (rs.next()) {
+                Prodotto p = new Prodotto();
+                p.setIdProdotto(rs.getInt("id_prodotto"));
+                p.setNome(rs.getString("nome"));
+                p.setMarca(rs.getString("marca"));
+                p.setPrezzoAttuale(rs.getDouble("prezzo_attuale"));
+                p.setCategoria(rs.getString("categoria"));
+                p.setGiacenza(rs.getInt("giacenza"));
+                
+             
+                
+                prodotti.add(p);
+            }
+        } catch (SQLException e) {
+            System.out.println("Errore in ProdottoDAO -> doRetrieveAll: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+        return prodotti;
+    }
 }
