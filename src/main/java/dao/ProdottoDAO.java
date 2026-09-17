33b7f46de884cff72442c9451cac9a1acfca023c
Antonio Liberti <antonioliberti2005@gmail.com>
2026-09-17 13:37:41 +0200

    Salvataggio carrello base

--- src/main/java/dao/ProdottoDAO.java
+++ src/main/java/dao/ProdottoDAO.java
@@ -12,31 +12,31 @@ import model.ConPool;
 
 public class ProdottoDAO {
 
-	public Prodotto doRetrieveById(int id) {
-	    // Adegua il nome della colonna 'id' se su phpMyAdmin si chiama diversamente!
-	    String query = "SELECT * FROM PRODOTTO WHERE id_prodotto = ? AND eliminato = false"; 
-	    try (java.sql.Connection con = model.ConPool.getConnection();
-	         java.sql.PreparedStatement ps = con.prepareStatement(query)) {
-	        
-	        ps.setInt(1, id);
-	        try (java.sql.ResultSet rs = ps.executeQuery()) {
-	            if (rs.next()) {
-	                Prodotto p = new Prodotto();
-	                p.setIdProdotto(rs.getInt("id")); // adegua il nome colonna
-	                p.setNome(rs.getString("nome"));
-	                p.setMarca(rs.getString("marca"));
-	                p.setPrezzoAttuale(rs.getDouble("prezzoAttuale")); // adegua il nome colonna
-	                p.setCategoria(rs.getString("categoria"));
-	                p.setGiacenza(rs.getInt("giacenza"));
-	                p.setEliminato(rs.getBoolean("eliminato"));
-	                return p;
-	            }
-	        }
-	    } catch (java.sql.SQLException e) {
-	        throw new RuntimeException("Errore SQL in doRetrieveById: " + e.getMessage());
-	    }
-	    return null;
-	}
+    public Prodotto doRetrieveById(int id) {
+        String query = "SELECT * FROM PRODOTTO WHERE id_prodotto = ? AND eliminato = false"; 
+        try (Connection con = ConPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+            
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    Prodotto p = new Prodotto();
+                    p.setIdProdotto(rs.getInt("id_prodotto")); // Corretto!
+                    p.setNome(rs.getString("nome"));
+                    p.setMarca(rs.getString("marca"));
+                    p.setPrezzoAttuale(rs.getDouble("prezzo_attuale")); // Corretto!
+                    p.setCategoria(rs.getString("categoria"));
+                    p.setGiacenza(rs.getInt("giacenza"));
+                    p.setEliminato(rs.getBoolean("eliminato"));
+                    return p;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Errore SQL in doRetrieveById: " + e.getMessage());
+        }
+        return null;
+    }
+
     public List<Prodotto> doRetrieveAll() {
         List<Prodotto> prodotti = new ArrayList<>();
         String query = "SELECT * FROM PRODOTTO WHERE eliminato = false";
@@ -53,8 +53,7 @@ public class ProdottoDAO {
                 p.setPrezzoAttuale(rs.getDouble("prezzo_attuale"));
                 p.setCategoria(rs.getString("categoria"));
                 p.setGiacenza(rs.getInt("giacenza"));
-                
-             
+                p.setEliminato(rs.getBoolean("eliminato"));
                 
                 prodotti.add(p);
             }
