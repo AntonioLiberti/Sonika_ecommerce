c9a691a7e977d71b4852670ccff81ae918ec9f9c
Antonio Liberti <antonioliberti2005@gmail.com>
2026-09-24 00:05:13 +0200

    aggiunta di AdminFilter e AdminServlet

---
+++ src/main/java/filter/AdminFilter.java
@@ -0,0 +1,37 @@
+package filter;
+
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Utente;
+
+@WebFilter("/Admin*")
+public class AdminFilter implements Filter {
+
+    public void init(FilterConfig fConfig) throws ServletException {}
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession(false);
+
+        Utente utente = (session != null) ? (Utente) session.getAttribute("utenteLoggato") : null;
+
+      
+        if (utente != null && "admin".equalsIgnoreCase(utente.getRuolo())) {
+            chain.doFilter(request, response);
+        } else {
+            res.sendRedirect(req.getContextPath() + "/login");
+        }
+    }
+
+    public void destroy() {}
+}
